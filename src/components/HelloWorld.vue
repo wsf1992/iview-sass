8d210f6acdec22207933b9a4b12f74fc7c31e00b
wangshaofeng <wangshaofeng@emicnet.com>
2018-07-05 11:29:32 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="box">
+    <p>h1下的p1</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped="" lang="scss" type="text/css">
+.box p {
+  font-size: 20px;
+  border : {
+    width: 20px;
+    color: red;
+    style: solid;
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+
+// 引入路由
+import router from './router'
+
+// 引入iview插件及样式
+import iView from 'iview'
+import "iview/dist/styles/iview.css"
+Vue.use(iView)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
